6f45b32641fbc2c716325f5be185df8bc312b34b
Rodrigo <rlopez@rlm.com.mx>
2015-08-08 11:16:34 -0500

    asasad

--- www/js/controllers/ctrl_home.js
+++ www/js/controllers/ctrl_home.js
@@ -51,7 +51,7 @@ var ctrl_home = {
 	getLocation: function(){
 		jqm.showLoader("localizando...");
 		//ctrl_home.map.locate({setView: true, maxZoom: 16});
-		navigator.geolocation.getCurrentPosition(ctrl_home.onLocationFound, ctrl_home.onLocationError,{maximumAge:3000,timeout:5000,enableHighAccuracy:true});
+		navigator.geolocation.getCurrentPosition(ctrl_home.onLocationFound, ctrl_home.onLocationError,{maximumAge:3000,timeout:35000,enableHighAccuracy:true});
     //ctrl_home.map.on('locationfound', ctrl_home.onLocationFound);
     //ctrl_home.map.on('locationerror', ctrl_home.onLocationError);
 	},
